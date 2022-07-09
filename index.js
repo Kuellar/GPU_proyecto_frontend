8d469860d45fa7d2e947a4220dd670ba39c50fc4
Kuellar <icuellartapia@gmail.com>
2022-07-08 22:22:11 -0400

    Add node

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
-import { vsSource, fsSource } from "./js/shaders.js";
+import {
+    Clock,
+    BoxGeometry,
+    Mesh,
+    MeshBasicMaterial,
+    Camera,
+    Scene,
+    WebGLRenderer,
+} from "three";
 
-var container;
-var camera, scene, renderer, clock;
-var uniforms;
+var container, camera, scene, renderer, clock;
 
 const init = () => {
     container = document.getElementById("threeJS");
 
-    camera = new THREE.Camera();
+    camera = new Camera();
     camera.position.z = 1;
 
-    scene = new THREE.Scene();
-    clock = new THREE.Clock();
+    scene = new Scene();
+    clock = new Clock();
 
-    var geometry = new THREE.PlaneBufferGeometry(2, 2);
+    var geometry = new BoxGeometry();
+    var material = new MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new Mesh(geometry, material);
+    scene.add(cube);
 
-    uniforms = {
-        // BASE
-        u_time: {
-            type: "f",
-            value: 1.0,
-        },
-        u_resolution: {
-            type: "v2",
-            value: new THREE.Vector2(),
-        },
-        u_mouse: {
-            type: "v2",
-            value: new THREE.Vector2(),
-        },
-    };
-
-    var material = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: vsSource,
-        fragmentShader: fsSource,
-    });
-
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer();
+    renderer = new WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
 
     container.appendChild(renderer.domElement);
 
     onWindowResize();
     window.addEventListener("resize", onWindowResize, false);
-
-    container.onmousemove = (e) => {
-        uniforms.u_mouse.value.x =
-            e.pageX * (window.innerWidth / window.innerHeight);
-        uniforms.u_mouse.value.y = -e.pageY + window.innerHeight;
-    };
 };
 
 const onWindowResize = (event) => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    uniforms.u_resolution.value.x = renderer.domElement.width;
-    uniforms.u_resolution.value.y = renderer.domElement.height;
 };
 
 const animate = () => {
@@ -67,7 +43,6 @@ const animate = () => {
 };
 
 const render = () => {
-    uniforms.u_time.value += clock.getDelta();
     renderer.render(scene, camera);
 };
 
